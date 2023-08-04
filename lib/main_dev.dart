6ee371d15ffa19c0bc400d8965b26708b1af5033
lim.chhily <lim.chhily17@gmail.com>
2023-08-04 17:05:16 +0700

    add geo and splash screen

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:travel_go/config/flavor.dart';
+import 'package:travel_go/provider/geo/geo_handler.dart';
 
 import 'main.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   FlavorConfig(flavor: Flavor.dev, values: FlavorValues(appName: "DEV"));
   runApp(const MyApp());
+  GeoHandler.handleLocationPermission();
+  FlutterNativeSplash.remove();
 }
